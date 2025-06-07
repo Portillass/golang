27aa429a954759d8e5e432824e806611234dab70
Jarell E. Portillas <2201107699@student.buksu.edu.ph>
2025-06-07 22:36:22 +0800

    Hello world!

---
+++ main.go
@@ -0,0 +1,7 @@
+package main // Main nga package, entry point sa atong programa
+
+import "fmt" // Import sa fmt nga package para sa output sa console
+
+func main() { // Main nga function, diri magsugod ang execution sa program
+    fmt.Println("Hello, World!") // I-print ang "Hello, World!" sa console
+}
